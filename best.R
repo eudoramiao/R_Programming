496257e43d50b7d005f476b4467fdf6263f35b45
eudoramiao <eudoramiao@gmail.com>
2019-07-28 18:10:56 -0500

    fixed Q2

--- best.R
+++ best.R
@@ -4,29 +4,31 @@
 
 best <- function(state, outcome) {
   ## Read outcome data
-  df <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-  ## Check taht state and outcome are valid
-  stateslist <- unique(df$State)
-  outcomelist = as.character(c("heart attack", "heart failure", "pneumonia"))
+  data <- read.csv("outcome-of-care-measures.csv")
+  
+  ## Check that state and outcome are valid
+  stateslist <- levels(data[, 7]) [data[, 7]]
   if (state %in% stateslist == FALSE) stop ("invalid state")
-  if (outcome %in% outcomelist == FALSE) stop ("invalid outcome")
+  if (!((outcome == "heart attack") | (outcome == "heart failure")
+        | (outcome == "pneumonia"))) {
+    stop ("invalid outcome")
+  }
+  
   ## Return hospital name in that state with lowest 30-day death rate
-  if (outcome == "heart attack"){
-    df1 <- subset(df, State == state, select = c("Hospital.Name", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"))
+  col <- if (outcome == "heart attack") {
+    11
+  } else if (outcome == "heart failure") {
+    17
+  } else {
+    23
   }
-  if (outcome == "heart failure"){
-      df1 <- subset(df, State == state, select = c("Hospital.Name", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"))
-  }
-  else{
-    df1 <- subset(df, State == state, select = c("Hospital.Name", "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"))
-  }
-  maxnum <- max(as.numeric(df1[,2]), na.rm = TRUE)
-  df3 <- subset(df, df[,2] == maxnum) 
-  df4 = sort(df3[,1])
-  output = df4
-  output
-} 
+  
+  data[, col] <- suppressWarnings(as.numeric(levels(data[, col])[data[, col]]))
+  data[, 2] <- as.character(data[, 2])
+  statedata <- data[grep(state, data$State), ]
+  orderdata <- statedata[order(statedata[, col], statedata[, 2], na.last = NA), ]
+  orderdata[1, 2]
+}
 
-best("TX", "heart attack")  
   
   
